6010f6cc1f6d5ee41940fb976c3576d3716f1c01
GYH <2350871838@qq.com>
2024-09-02 20:28:06 +0800

    更新sql

--- student_manage.sql
+++ student_manage.sql
@@ -11,13 +11,81 @@
  Target Server Version : 80033 (8.0.33)
  File Encoding         : 65001
 
- Date: 31/08/2024 00:25:56
+ Date: 02/09/2024 20:27:05
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
-CREATE DATABASE `student_manage` CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_bin';
+-- ----------------------------
+-- Table structure for exam_question
+-- ----------------------------
+DROP TABLE IF EXISTS `exam_question`;
+CREATE TABLE `exam_question`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `describe` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '描述',
+  `score` float NOT NULL COMMENT '得分',
+  `create_at` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '创建者',
+  `create_time` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  `multiple_choice` blob NULL COMMENT '是否多选',
+  `no` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '题目编号',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of exam_question
+-- ----------------------------
+INSERT INTO `exam_question` VALUES (1, '1+1等于多少', 0.5, '123', '2024-08-31 21:37:09', 0x30, '8月模拟卷');
+INSERT INTO `exam_question` VALUES (2, '1*1等于多少', 1, '123', '2024-08-31 21:37:38', 0x30, '8月模拟卷');
+INSERT INTO `exam_question` VALUES (3, '下列属于危险行为的是', 10, '123', '2024-08-31 21:38:27', 0x31, '8月模拟卷');
+
+-- ----------------------------
+-- Table structure for message
+-- ----------------------------
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE `message`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `label` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '标签',
+  `verbal_trick` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '话术',
+  `describe` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '描述',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of message
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for option
+-- ----------------------------
+DROP TABLE IF EXISTS `option`;
+CREATE TABLE `option`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `question_id` int NULL DEFAULT NULL COMMENT '题目id',
+  `option_description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '选项',
+  `answers` blob NULL COMMENT '是否是正确答案',
+  `no` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '编号',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `question_id`(`question_id` ASC) USING BTREE,
+  CONSTRAINT `option_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `exam_question` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 14 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of option
+-- ----------------------------
+INSERT INTO `option` VALUES (1, 1, '2', 0x31, 'A');
+INSERT INTO `option` VALUES (2, 1, '3', 0x30, 'B');
+INSERT INTO `option` VALUES (3, 1, '1', 0x30, 'C');
+INSERT INTO `option` VALUES (4, 1, '没有正确答案', 0x30, 'D');
+INSERT INTO `option` VALUES (5, 2, '2', 0x30, 'A');
+INSERT INTO `option` VALUES (6, 2, '3', 0x30, 'B');
+INSERT INTO `option` VALUES (7, 2, '1', 0x31, 'C');
+INSERT INTO `option` VALUES (8, 2, '0', 0x30, 'D');
+INSERT INTO `option` VALUES (9, 3, '游泳', 0x30, 'A');
+INSERT INTO `option` VALUES (10, 3, '打架', 0x31, 'B');
+INSERT INTO `option` VALUES (11, 3, '闯红灯', 0x31, 'C');
+INSERT INTO `option` VALUES (12, 3, '跑步', 0x30, 'D');
+INSERT INTO `option` VALUES (13, 3, '唱《鸡你太美》', 0x31, 'E');
 
 -- ----------------------------
 -- Table structure for role
@@ -50,7 +118,7 @@ CREATE TABLE `user`  (
   `id` int NOT NULL,
   `username` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '用户名',
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '密码',
-  `enable` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '是否启用',
+  `enable` blob NOT NULL COMMENT '是否启用',
   `create_time` datetime NOT NULL COMMENT '创建时间',
   `tel` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '手机号',
   `email` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '邮箱',
@@ -62,7 +130,31 @@ CREATE TABLE `user`  (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'admin', '{bcrypt}$2a$10$oVSzGSlieTXrO2xmsqnegOwJJjVaIHhGRnZHm25yaiP0UvORJJn36', 't', '2023-04-27 23:06:51', NULL, NULL, NULL, '123');
+INSERT INTO `user` VALUES (1, 'admin', '{bcrypt}$2a$10$oVSzGSlieTXrO2xmsqnegOwJJjVaIHhGRnZHm25yaiP0UvORJJn36', 0x74, '2023-04-27 23:06:51', NULL, NULL, NULL, '123');
+
+-- ----------------------------
+-- Table structure for user_option
+-- ----------------------------
+DROP TABLE IF EXISTS `user_option`;
+CREATE TABLE `user_option`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL COMMENT '用户id',
+  `option_id` int NOT NULL COMMENT '选项id',
+  `create_time` timestamp NOT NULL COMMENT '做题时间',
+  `question_id` int NOT NULL COMMENT '题目id',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `user_id`(`user_id` ASC) USING BTREE,
+  CONSTRAINT `user_option_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user_option
+-- ----------------------------
+INSERT INTO `user_option` VALUES (3, 1, 2, '2024-08-31 21:56:06', 1);
+INSERT INTO `user_option` VALUES (4, 1, 7, '2024-08-31 22:13:11', 2);
+INSERT INTO `user_option` VALUES (5, 1, 10, '2024-08-31 22:13:33', 3);
+INSERT INTO `user_option` VALUES (6, 1, 11, '2024-08-31 22:13:45', 3);
+INSERT INTO `user_option` VALUES (7, 1, 13, '2024-08-31 22:13:50', 3);
 
 -- ----------------------------
 -- Table structure for user_role
